7d1757f383258d937afa58a2913b6bace0b0b886
Michael Griffin <mdgriffin064@gmail.com>
2018-10-09 21:33:19 +0100

    Added REST endpoint to get one Reference

--- src/main/java/com/mdgriffin/referenceminder/controller/ReferenceController.java
+++ src/main/java/com/mdgriffin/referenceminder/controller/ReferenceController.java
@@ -1,9 +1,11 @@
 package com.mdgriffin.referenceminder.controller;
 
 import com.mdgriffin.referenceminder.entity.Reference;
+import com.mdgriffin.referenceminder.exception.ResourceNotFoundException;
 import com.mdgriffin.referenceminder.service.ReferenceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,11 @@ public class ReferenceController {
     @GetMapping("/references")
     public List<Reference> getReferences(){
         return referenceService.getReferences();
+    }
+
+    @GetMapping("/references/{referenceId}")
+    public Reference getOneReference (@PathVariable String referenceId) {
+        return referenceService.findById(referenceId).orElseThrow(() -> new ResourceNotFoundException("Reference", "id", referenceId));
     }
 
 }
