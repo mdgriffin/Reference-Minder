13fb86edc14e7c88b4fbe891690c02f837bb9e39
Michael Griffin <mdgriffin064@gmail.com>
2018-10-09 21:41:30 +0100

    Added REST endpoint to post and insert reference

--- src/main/java/com/mdgriffin/referenceminder/controller/ReferenceController.java
+++ src/main/java/com/mdgriffin/referenceminder/controller/ReferenceController.java
@@ -4,11 +4,10 @@ import com.mdgriffin.referenceminder.entity.Reference;
 import com.mdgriffin.referenceminder.exception.ResourceNotFoundException;
 import com.mdgriffin.referenceminder.service.ReferenceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.sql.Ref;
 import java.util.List;
 
 @RestController
@@ -26,6 +25,12 @@ public class ReferenceController {
     @GetMapping("/references/{referenceId}")
     public Reference getOneReference (@PathVariable String referenceId) {
         return referenceService.findById(referenceId).orElseThrow(() -> new ResourceNotFoundException("Reference", "id", referenceId));
+    }
+
+    @PostMapping("/references")
+    public Reference createReference(@RequestBody @Valid Reference reference) {
+        return referenceService.saveReference(reference);
+
     }
 
 }
