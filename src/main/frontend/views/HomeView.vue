b0dddd933a1732087d75bf02bc0201babdde790e
Michael Griffin <mdgriffin064@gmail.com>
2018-10-11 21:54:07 +0100

    Imported Datatable component and converted to single page component

--- src/main/frontend/views/HomeView.vue
+++ src/main/frontend/views/HomeView.vue
@@ -2,6 +2,9 @@
   <div class="home">
     <h1>Home Page</h1>
     <h2>References</h2>
+      <div v-if="referenceRows">
+          <data-table :headings="referenceHeadings" :rows="referenceRows"></data-table>
+      </div>
     <reference-list :references="references"></reference-list>
     <reference-form :reference="emptyReference" @save="onSaveReference"></reference-form>
   </div>
@@ -10,13 +13,15 @@
 <script>
 import ReferenceForm from '../components/ReferenceForm.vue';
 import ReferenceList from '../components/ReferenceList.vue';
+import DataTable from '../components/DataTable.vue';
 import {getReferences, saveReference} from "../api/reference-api";
 
 export default {
     name: 'home-view',
     components: {
         'reference-form': ReferenceForm,
-        'reference-list': ReferenceList
+        'reference-list': ReferenceList,
+        'data-table': DataTable
     },
     data() {
         return {
@@ -31,12 +36,36 @@ export default {
                 },
                 date: null,
                 dateAccessed: null
-            }
+            },
+            referenceHeadings: ['Title', 'Pages From', 'Pages To']
         }
     },
     asyncComputed: {
         references () {
             return getReferences();
+        },
+        referenceRows () {
+            let self = this;
+            return new Promise ((resolve, reject) => {
+                getReferences().then(references => {
+                    let res = [];
+
+                    references.forEach(reference => {
+                        let arr = [];
+
+                        arr.push(reference.title);
+                        arr.push(reference.pages.from)
+                        arr.push(reference.pages.to)
+
+                        res.push(arr)
+                    })
+
+                    resolve(res)
+                })
+                .catch(err => {
+                    reject(err);
+                })
+            });
         }
     },
     methods: {
