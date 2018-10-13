f33b0bf21ee9644c7a98362fb70ab42dd4603508
Michael Griffin <mdgriffin064@gmail.com>
2018-10-13 22:12:25 +0100

    Moved modal into home view

--- src/main/frontend/views/HomeView.vue
+++ src/main/frontend/views/HomeView.vue
@@ -13,7 +13,9 @@
                     </template>
                 </data-table>
             </div>
-            <reference-form-modal :reference="emptyReference" @save="onSaveReference"></reference-form-modal>
+            <modal name="new-reference-form" transition="pop-out" :width="modalWidth" :height="modalHeight" :scrollable="modalScroll">
+                <reference-form :reference="emptyReference" @save="onSaveReference"></reference-form>
+            </modal>
             <button @click="showCreateForm" class="btn btn-large btn-secondary">Add Reference</button>
         </div>
   </div>
@@ -23,7 +25,6 @@
 import ReferenceForm from '../components/ReferenceForm.vue';
 import ReferenceList from '../components/ReferenceList.vue';
 import DataTable from '../components/DataTable.vue';
-import ReferenceFormModal from '../components/ReferenceFormModal.vue';
 import TagList from "../components/TagList.vue";
 import {getReferences, getReferenceTags, saveReference, deleteReference} from "../api/reference-api";
 import {flatten} from '../filters/references-filter.js'
@@ -31,10 +32,9 @@ import {flatten} from '../filters/references-filter.js'
 export default {
     name: 'home-view',
     components: {
-        'reference-form': ReferenceForm,
+        ReferenceForm,
         'reference-list': ReferenceList,
         'data-table': DataTable,
-        ReferenceFormModal,
         TagList
     },
     data() {
@@ -52,7 +52,10 @@ export default {
                 dateAccessed: null
             },
             referenceHeadings: ['Title', 'Authors', 'Pages From', 'Pages To', 'Date', 'Date Accessed', 'Type'],
-            numReferenceSaves: 0
+            numReferenceSaves: 0,
+            modalWidth: '60%',
+            modalHeight: 'auto',
+            modalScroll: true
         }
     },
     filters: {
@@ -97,7 +100,7 @@ export default {
                 })
         },
         showCreateForm () {
-            this.$modal.show('reference-form-modal')
+            this.$modal.show('new-reference-form')
         }
     }
 }
